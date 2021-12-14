7f3d8264cb446a3866cfbd346baf22a96e4a561b
luffluo <luffluo@outlook.com>
2021-12-14 15:18:15 +0800

    Update string practice for KMP

--- string/main.c
+++ string/main.c
@@ -64,7 +64,7 @@ int str_index1(string *str, string *sub)
     return -1;
 }
 
-int str_index(string *str, string *sub)
+int str_index2(string *str, string *sub)
 {
     int k = 0;
     int i = k, j = k;
@@ -85,6 +85,35 @@ int str_index(string *str, string *sub)
             }
 
             j = 0;
+        }
+    }
+    
+    if (j == sub->length) {
+        return k;
+    }
+
+    return -1;
+}
+
+int str_index(string *str, string *sub)
+{
+    int k = 0;
+    int i = k, j = k;
+    int valid_index = str->length - sub->length + 1;
+
+    while (i < str->length && j < sub->length) {
+        if (str->chars[i] == sub->chars[j]) {
+            i++;
+            j++;
+        } else {
+            k++;
+            j = 0;
+
+            // 如果最后的字符不够字串的长度
+            // 就结束匹配
+            if (i > valid_index) {
+                break;
+            }
         }
     }
     
